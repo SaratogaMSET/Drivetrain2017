0393342798a3f1d56e6d52e6c3de5652636a11d4
Valen <3.14159.a.z.y@gmail.com>
2017-01-28 00:49:52 -0800

    TurretPID

--- src/org/usfirst/frc/team649/robot/subsystems/TurretSubsystem.java
+++ src/org/usfirst/frc/team649/robot/subsystems/TurretSubsystem.java
@@ -18,19 +18,19 @@ public class TurretSubsystem extends PIDSubsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	public static class PIDConstants {
-		public static final double k_P = 0.0;
+		public static final double k_P = 0.1;
 		public static final double k_I = 0.0;
-		public static final double k_D = 0.0;
+		public static final double k_D = 0.05;
 		
-		public static final double ENCODER_DEGREES_PER_PULSE = 0.0;
+		public static final double ENCODER_DEGREES_PER_PULSE = 360.0 / 256.0
+				* 200.0/18.0; // uhh ... guessed;
 		
 		public static final double MAX_MOTOR_OUTPUT = 0.05;
 		public static final double MIN_MOTOR_OUTPUT = 0.01;
-		public static final double MAX_MOTOR_DOWN_POWER = 0.0;
 		public static final double ABSOLUTE_TOLERANCE = 0.05;
-		
-		public static double max_motor_up_power = 0.6;
 	}
+	public static final double ENCODER_CIRCUMFERENCE = 5.5*Math.PI;
+	
 	public Victor turretMotor;
 	public Encoder turretShaftEncoder;
 	public PIDController pid;
@@ -59,12 +59,28 @@ public class TurretSubsystem extends PIDSubsystem {
 	}
 	
 	public boolean isOnTarget(double setpoint) {
-		return (setpoint-turretShaftEncoder.getDistance()) > PIDConstants.ABSOLUTE_TOLERANCE;
+		return (Math.abs(setpoint-turretShaftEncoder.getDistance())) < PIDConstants.ABSOLUTE_TOLERANCE;
 	}
 	
 	public void setHoodAngle(double angle) {
 		angleOfShooter = angle;
 		hoodServo.setAngle(angleOfShooter);
+	}
+	
+	public void resetEncoders() {
+		turretShaftEncoder.reset();
+	}
+	
+	public double getClosestAngleToSetpoint(double setpoint) {
+		double diff1 = Math.abs(turretShaftEncoder.getDistance()-setpoint);
+		double diff2 = Math.abs(ENCODER_CIRCUMFERENCE-setpoint);
+		if (diff1 > diff2) {
+			return diff2;
+		} 
+		else if (diff2 > diff1) {
+			return diff1;
+		}
+		return diff1;
 	}
 
     public void initDefaultCommand() {
@@ -74,7 +90,7 @@ public class TurretSubsystem extends PIDSubsystem {
 	@Override
 	protected double returnPIDInput() {
 		// TODO Auto-generated method stub
-		return 0;
+		return getClosestAngleToSetpoint(this.pid.getSetpoint());
 	}
 	@Override
 	protected void usePIDOutput(double output) {
